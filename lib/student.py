532f1b223b0ad99721ee5cf1f4c6608c5623973e
mararo-ops <danielmararo1@gmail.com>
2023-12-01 01:45:22 +0300

    complete assignment

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
 from user import User
-
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []  # Initialize with an empty list
+
+    def learn(self, new_knowledge):
+        # Add the provided string to the student's self.knowledge list
+        self.knowledge.append(new_knowledge)
+s
